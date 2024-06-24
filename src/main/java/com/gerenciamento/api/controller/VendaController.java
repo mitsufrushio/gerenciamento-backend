f4f3137d5b8e793d8f2ab35b4134d53b53aaf02d
mitsufrushio <mitsu.junior1215@outlook.com>
2024-06-23 23:40:38 -0300

    Inclusão de processo de upload de Venda

--- src/main/java/com/gerenciamento/api/controller/VendaController.java
+++ src/main/java/com/gerenciamento/api/controller/VendaController.java
@@ -11,59 +11,48 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.gerenciamento.api.domain.Cliente;
-import com.gerenciamento.api.service.ClienteService;
-import com.gerenciamento.processplans.ClienteProcessPlan;
+import com.gerenciamento.api.domain.Venda;
+import com.gerenciamento.api.repository.VendaRepository;
+import com.gerenciamento.processplans.VendaProcessPlan;
 
-@RequestMapping("/cliente")
+@RequestMapping("/venda")
 @RestController
-public class ClienteController {
+public class VendaController {
 	
 	@Autowired
-	ClienteService clienteService;
+	VendaRepository vendaRepository;
 	
 	@Autowired
-	ClienteProcessPlan clienteProcessPlan;
-	
-	@PostMapping("/create")
-	public ResponseEntity<Cliente> handlePost(@RequestBody Cliente cliente) {
-		try {
-			clienteService.save(cliente);
-			return new ResponseEntity<>(HttpStatus.CREATED);
-		} catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-	}	
-	
-	@PostMapping("/upload")
-	public ResponseEntity<Cliente> upload(@RequestParam("file") MultipartFile multipartFile) {
-		try {
-			File file = new File(System.getProperty("java.io.tmpdir") + "/" + multipartFile.getOriginalFilename());
-			multipartFile.transferTo(file);
-			clienteProcessPlan.processPlan(file);
-			return new ResponseEntity<>(HttpStatus.CREATED);
-		} catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-	}	
+	VendaProcessPlan vendaProcessPlan;
 	
 	@GetMapping("/all")
-	public ResponseEntity<List<Cliente>> all() {
-		List<Cliente> list = clienteService.findAll();
+	public ResponseEntity<List<Venda>> all() {
+		List<Venda> list = vendaRepository.findAll();
 		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 	
-	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<Cliente> delete(@PathVariable("id") Long id){
+	@PostMapping("/upload")
+	public ResponseEntity<Venda> upload(@RequestParam("file") MultipartFile multipartFile) {
 		try {
-			Optional<Cliente> cliente = clienteService.findById(id);
-			clienteService.delete(cliente.get());
+			File file = new File(System.getProperty("java.io.tmpdir") + "/" + multipartFile.getOriginalFilename());
+			multipartFile.transferTo(file);
+			vendaProcessPlan.processPlan(file);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Venda> delete(@PathVariable("id") Long id){
+		try {
+			Optional<Venda> cliente = vendaRepository.findById(id);
+			vendaRepository.delete(cliente.get());
 			return new ResponseEntity<>(HttpStatus.OK);
 		} catch(Exception e) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
